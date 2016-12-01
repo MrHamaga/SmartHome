fba560dece8ccc2f374784d412d338fdc201d9a4
MrHamaga <tore.nygaard@gmail.com>
2016-12-01 12:54:01 +0100

    Sonos: Added first version of a Sonos controller which can play and pause one speaker.

---
+++ SonosControl/SonosApi/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+
+namespace SonosApi.Helpers
+{
+    public class ConfigurationHelper
+    {
+        internal static string GetSonosIp()
+        {
+            return GetStringFromConfiguration("SonosIp", false);
+        }
+
+        private static string GetStringFromConfiguration(string key, bool optional)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                return value;
+            }
+            if (optional)
+            {
+                return "";
+            }
+            else
+            {
+                throw new ArgumentNullException("key");
+            }
+        }
+
+        void dd()
+        {
+
+        }
+    }
+}
---
+++ SonosControl/SonosApi/SonosRequestHelper.cs
@@ -0,0 +1,22 @@
+﻿using SonosApi.Helpers;
+
+namespace SonosApi
+{
+    public class SonosRequestHelper
+    {
+        SonosSoapClient s;
+        public SonosRequestHelper()
+        {
+            s = new SonosSoapClient(string.Format("http://{0}/MediaRenderer/AVTransport/Control", ConfigurationHelper.GetSonosIp()));
+        }
+
+        public void Pause()
+        {
+            s.Pause(0);
+        }
+       
+        public void Play() {
+            s.Play(0);
+        }
+    }
+}
